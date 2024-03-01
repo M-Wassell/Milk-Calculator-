65c5bfa8081b6980de048755ef803287c779e75e
M-Wassell <55836302+M-Wassell@users.noreply.github.com>
2024-03-01 14:59:32 +0000

    Function call complete

---
+++ milkBottles.js
@@ -0,0 +1,29 @@
+
+
+function getMilk(money, milkCost){
+    console.log(`Hi, the number of bottles you can buy for £${money} is ` + bottleCount(money, milkCost)+ ` bottles`);
+    return console.log(`Hello, your change is £` + change(money, milkCost));
+}
+
+function change(money, milkCost){
+
+    var theChange = money % milkCost;
+    return theChange;
+}
+
+function bottleCount(money, milkCost){
+    var bottleNumber = Math.floor(money/milkCost)
+    return bottleNumber;
+}
+
+getMilk(100, 40);
+
+
+function bmiCalculator(weight, height) {
+  let bmi = weight / Math.pow(height, 2);
+
+  return bmi < 18.5 ? `Your BMI is ${bmi}, so you are underweight.`: bmi >= 18.5 && bmi <= 24.9 ? 
+    `Your BMI is ${bmi}, so you have a normal weight.`: `Your BMI is ${bmi}, so you are overweight.`;
+}
+
+console.log(bmiCalculator(19, 2));
